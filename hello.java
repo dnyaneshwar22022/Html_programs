b037c9c474f8e6921c912621ae5e568a61aa0693
dnyaneshwar22022 <dnyaneshmavaskar@gmail.com>
2025-03-01 09:48:29 -0800

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,9 @@
+public class hello{
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        int n = 100;
+        for(int i=0; i<=n; i++) {
+            System.out.println(i);
+        }
+    }
+}
